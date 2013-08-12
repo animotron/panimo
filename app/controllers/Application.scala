cbb0b9f99aacf2534ba44a8583d5e9942c9ad7bc
Vasilii Startcev <wstarcev@gmail.com>
2013-08-13 03:54:30 +0400

    add neo4j dependency

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,8 +2,11 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.neo4j.kernel._
 
 object Application extends Controller {
+
+  var neo4j = new EmbeddedGraphDatabase("data")
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -12,5 +15,6 @@ object Application extends Controller {
   def users = Action {
     Ok(views.html.users())
   }
+
 
 }
--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,13 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     //cache
+    "org.neo4j" % "neo4j-kernel" % "1.9.2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
+//    "org.neo4j" % "neo4j-kernel" % "1.9.RC1"
   )
 
 }
